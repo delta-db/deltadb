321f8bec916713c2d4f8ffcd9aa5a103b416d7d6
Geoff Cox <redgeoff@gmail.com>
2015-12-06 17:31:52 +0000

    feat(config): separate namespaces

--- test/node/index.js
+++ test/node/index.js
@@ -9,4 +9,12 @@ chai.should();
 // delicate.
 require('../../scripts/auto-adapter-store');
 
+// Set config so that our test server doesn't interfere with any production server. We need to set
+// the config first so that all of the following code uses this config.
+var config = require('../../scripts/config'),
+  testConfig = require('../config');
+for (var i in testConfig) {
+  config[i] = testConfig[i];
+}
+
 require('../spec');
