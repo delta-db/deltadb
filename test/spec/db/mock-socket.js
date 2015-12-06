a0af4260204d4424ce242c4ce431c641e21f198e
Geoff Cox <redgeoff@gmail.com>
2015-12-06 02:15:43 +0000

    test(db): mock db create/destroy

--- test/spec/db/mock-socket.js
+++ test/spec/db/mock-socket.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var inherits = require('inherits'),
-  EventEmitter = require('events').EventEmitter;
+  EventEmitter = require('events').EventEmitter,
+  clientUtils = require('../../../scripts/utils'),
+  DBMissingError = require('deltadb-common-utils/scripts/errors/db-missing-error');
 
-var MockSocket = function () {};
+var MockSocket = function () {
+  this._dbs = {};
+  this._dbs[clientUtils.SYSTEM_DB_NAME] = true; // system should always be present
+};
 
 inherits(MockSocket, EventEmitter);
 
@@ -14,18 +19,73 @@ MockSocket.prototype.connect = function ( /* url */ ) {
   });
 };
 
-MockSocket.prototype.emit = function (event/* , msg */) {
-
-  switch (event) {
-  case 'init':
+MockSocket.prototype._processInit = function (msg) {
+  if (this._dbs[msg.db]) { // db exists?
     this.emit('init-done');
+  } else { // db missing
+    this.emit('delta-error', new DBMissingError(msg.db + ' missing'));
+  }
+};
+
+MockSocket.prototype._processDBAction = function (change) {
+  var val = JSON.parse(change.val);
+
+  switch (val.action) {
+  case clientUtils.ACTION_ADD:
+
     break;
+
+  case clientUtils.ACTION_REMOVE:
+
+    break;
+  }
+};
+
+MockSocket.prototype._processAction = function (change) {
+  switch (change.col) {
+  case clientUtils.DB_COLLECTION_NAME:
+    this._processDBAction(change);
+    break;
+  }
+};
+
+MockSocket.prototype._processChange = function (change) {
+  switch (change.name) {
+  case clientUtils.ATTR_NAME_ACTION:
+    this._processAction(change);
+    break;
+  }
+};
+
+MockSocket.prototype._processChanges = function (msg) {
+  var self = this;
+  msg.changes.forEach(function (change) {
+    self._processChange(change);
+    change.re = (new Date()).toISOString(); // simulate recording
+  });
+
+  // Simulate recording
+  this.emit('changes', msg, true);
+};
+
+MockSocket.prototype.emit = function (event, msg, fromServer) {
+
+  if (!fromServer) { // coming from client?
+    switch (event) {
+    case 'init':
+      this._processInit(msg);
+      break;
+
+    case 'changes':
+      this._processChanges(msg);
+      break;
+    }
   }
 
   return EventEmitter.prototype.emit.apply(this, arguments);
 };
 
-MockSocket.prototype.on = function (/* event, callback */) {
+MockSocket.prototype.on = function ( /* event, callback */ ) {
   return EventEmitter.prototype.on.apply(this, arguments);
 };
 
